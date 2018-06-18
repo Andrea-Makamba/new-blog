5bc9660870ae8908500a3e785c153359b86ddb99
Andrea Makamba <andymak@seas.upenn.edu>
2018-06-18 13:14:13 +0200

    not working save

--- intro.R
+++ intro.R
@@ -2,11 +2,9 @@
 library(blogdown)
 new_site() 
 
-install_theme("hugo-nederburg-theme", theme_example = FALSE, update_config = TRUE, force = TRUE)
-
 
 # make/view website with new theme
 serve_site() # then hit stop
 
-new_post("Factorial, The Package You Need", ext = ".Rmd")
+#new_post("Factorial, The Package You Need", ext = ".Rmd")
 
